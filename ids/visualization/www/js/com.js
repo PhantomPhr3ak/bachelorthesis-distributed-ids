62045e6ca12005391b4b03ad1323004a7d89271e
Kai Grosshanten <kgrossha@uni-muenster.de>
2022-07-25 22:08:33 +0200

    fix url from websocket

--- ids/visualization/www/js/com.js
+++ ids/visualization/www/js/com.js
@@ -4,7 +4,8 @@ function initWebsocket() {
 
     // Save bandwidth by storing the ID of the last report of a violation
     let lastTimestamp = -1;
-    let socket = new WebSocket("ws://localhost:8777");
+    //TODO change url to c2
+    let socket = new WebSocket("ws://raspberrypi1:8777");
 
     // event triggered when websocket is connected
     socket.onopen = function (e) {
@@ -13,7 +14,8 @@ function initWebsocket() {
         setInterval(function () {
             // Automatically retry connecting to C2 websocket when connection is lost / closed
             if (socket.readyState === WebSocket.CLOSED) {
-                socket = new WebSocket("ws://localhost:8777");
+                //TODO change url to c2
+                socket = new WebSocket("ws://raspberrypi1:8777");
                 return;
             }
             

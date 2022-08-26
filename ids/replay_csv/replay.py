0381af83b925e977489b31592536f03b28690251
Kai Grosshanten <kgrossha@uni-muenster.de>
2022-08-26 22:44:06 +0200

    Add basic structure of the new "csv replay mode" based on the scenarios of Verena Menzel and modbusRTU.

---
+++ ids/replay_csv/replay.py
@@ -0,0 +1,16 @@
+import threading
+
+from ids.deployment.testbed.mosaikrtu.dvcd.data import DataBlock
+from ids.deployment.testbed.mosaikrtu.dvcd.server import Server
+
+def replayScenario(i):
+    t1 = threading.Thread()
+    t2 = threading.Thread()
+
+    server0 = Server()
+    server1 = Server()
+
+
+
+if __name__ == '__main__':
+    replayScenario()
